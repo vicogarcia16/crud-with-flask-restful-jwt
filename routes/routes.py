cd397387d7d252454cb2db0921b3d44a9cd2d8dd
Victor García <mg_619@hotmail.com>
2022-05-09 11:57:34 -0500

    Modificación de rutas

--- routes/routes.py
+++ routes/routes.py
@@ -1,11 +1,22 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, render_template
 from routes.movies import MoviesApi, MovieApi 
 from auth.auth import Login, Refresh
 user = Blueprint('user', __name__)
 
 @user.route('/user/<name>')
 def get_user(name):
-    return jsonify({'Usuario': name})
+    data= [
+        {'id':1, 'texto': 'dato'},
+        {'id':2,'texto': 'dato2'},
+        {'id':3, 'texto': 'dato3'}
+    ]
+
+    enlaces=[{"url":"http://www.google.es","texto":"Google"},
+			{"url":"http://www.twitter.com","texto":"Twitter"},
+			{"url":"http://www.facbook.com","texto":"Facebook"},
+			]
+
+    return render_template('index.html', name=name, data=data, enlaces=enlaces)
 
 
 def initialize_routes(api):
